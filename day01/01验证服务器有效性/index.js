a20c18ae3a3e990bdecc4107365d5cc6264626ce
lixiao <lixiao@zmdtech.com>
2019-08-19 14:16:37 +0800

    初始化项目

---
+++ day01/01验证服务器有效性/index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const sha1 = require('sha1')
+
+const app = express()
+
+const config = {
+  token: 'shawleeHTML20190818',
+  appID: 'wx402bfd91eede695e',
+  appsecret: 'ecd3012be29b8d69ea323b488301c4df'
+}
+
+app.use((req, res, next) => {
+  //微信服务器提交的参数
+  console.log(req.query)
+  // { signature: 'f534b0f9ab808b77300cb41002b03c6bec513689', //微信的加密签名
+  //   echostr: '4739002693897355243',  //微信的随机字符串
+  //   timestamp: '1566182859',  //微信发送请求的时间戳
+  //   nonce: '1457731178' }  //微信的随机数字
+
+  /**
+   * 验证微信签名
+   */
+  //1、将参与微信加密签名的三个参数（timestamp、nonce、token）按照字典序排序并组合成数组
+  const { signature,echostr,timestamp,nonce } = req.query
+  const { token } = config
+
+  const arr = [timestamp, nonce, token]
+  const sortArr = arr.sort()
+
+  //2、将组合的数组拼接成字符串并进行sha1加密
+  const sha1Str = sha1(sortArr.join(''))
+
+  //3、将加密完成的字符串和微信的signature对比
+  if (sha1Str === signature) {
+    //如果一样，说明消息来自于服务器，返回echostr给微信服务器
+    res.send(echostr)
+  } else {
+    //如果不一样，说明不是微信服务器发送的消息，返回error
+    res.end('error')
+  }
+})
+
+app.listen(8080, () => console.log('服务器启动成功~'))
---
+++ day01/02模块化/config/index.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+  token: 'shawleeHTML20190818',
+  appID: 'wx402bfd91eede695e',
+  appsecret: 'ecd3012be29b8d69ea323b488301c4df'
+}
